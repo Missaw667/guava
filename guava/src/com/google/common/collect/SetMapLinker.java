b10cac07c2f5e304fe542526536db6cde50baf04
Wassim LaMesker <wassim.cherabi.etu@univ-lille.fr>
2026-04-05 20:56:43 +0200

    Petite Modification : Réorganisation de SetMapLinker

--- guava/src/com/google/common/collect/SetMapLinker.java
+++ guava/src/com/google/common/collect/SetMapLinker.java
@@ -17,6 +17,83 @@ final class SetMapLinker {
     private SetMapLinker() {}
     private static final double LOAD_FACTOR = 0.75;
 
+    /**
+     * Returns the elements of {@code unfiltered} that satisfy a predicate. The returned set is a live
+     * view of {@code unfiltered}; changes to one affect the other.
+     *
+     * <p>The resulting set's iterator does not support {@code remove()}, but all other set methods
+     * are supported. When given an element that doesn't satisfy the predicate, the set's {@code
+     * add()} and {@code addAll()} methods throw an {@link IllegalArgumentException}. When methods
+     * such as {@code removeAll()} and {@code clear()} are called on the filtered set, only elements
+     * that satisfy the filter will be removed from the underlying set.
+     *
+     * <p>The returned set isn't threadsafe or serializable, even if {@code unfiltered} is.
+     *
+     * <p>Many of the filtered set's methods, such as {@code size()}, iterate across every element in
+     * the underlying set and determine which elements satisfy the filter. When a live view is
+     * <i>not</i> needed, it may be faster to copy {@code Iterables.filter(unfiltered, predicate)} and
+     * use the copy.
+     *
+     * <p><b>Warning:</b> {@code predicate} must be <i>consistent with equals</i>, as documented at
+     * {@link Predicate#apply}. Do not provide a predicate such as {@code
+     * Predicates.instanceOf(ArrayList.class)}, which is inconsistent with equals. (See {@link
+     * Iterables#filter(Iterable, Class)} for related functionality.)
+     *
+     * <p><b>Java 8+ users:</b> many use cases for this method are better addressed by {@link
+     * java.util.stream.Stream#filter}. This method is not being deprecated, but we gently encourage
+     * you to migrate to streams.
+     */
+    // TODO(kevinb): how to omit that last sentence when building GWT javadoc?
+    public static <E extends @Nullable Object> Set<E> filter(
+            Set<E> unfiltered, Predicate<? super E> predicate) {
+        if (unfiltered instanceof SortedSet) {
+            return filter((SortedSet<E>) unfiltered, predicate);
+        }
+        if (unfiltered instanceof Sets.FilteredSet) {
+            // Support clear(), removeAll(), and retainAll() when filtering a filtered
+            // collection.
+            Sets.FilteredSet<E> filtered = (Sets.FilteredSet<E>) unfiltered;
+            Predicate<E> combinedPredicate = com.google.common.base.Predicates.and(filtered.predicate, predicate);
+            return new Sets.FilteredSet<>((Set<E>) filtered.unfiltered, combinedPredicate);
+        }
+
+        return new Sets.FilteredSet<>(checkNotNull(unfiltered), checkNotNull(predicate));
+    }
+
+    /**
+     * Returns an unmodifiable view of the specified navigable set. This method allows modules to
+     * provide users with "read-only" access to internal navigable sets. Query operations on the
+     * returned set "read through" to the specified set, and attempts to modify the returned set,
+     * whether direct or via its collection views, result in an {@code UnsupportedOperationException}.
+     *
+     * <p>The returned navigable set will be serializable if the specified navigable set is
+     * serializable.
+     *
+     * <p><b>Java 8+ users and later:</b> Prefer {@link Collections#unmodifiableNavigableSet}.
+     *
+     * @param set the navigable set for which an unmodifiable view is to be returned
+     * @return an unmodifiable view of the specified navigable set
+     * @since 12.0
+     */
+    public static <E extends @Nullable Object> NavigableSet<E> unmodifiableNavigableSet(
+            NavigableSet<E> set) {
+        if (set instanceof ImmutableCollection || set instanceof Sets.UnmodifiableNavigableSet) {
+            return set;
+        }
+        return new Sets.UnmodifiableNavigableSet<>(set);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return super.retainAll(checkNotNull(c)); // GWT compatibility
+    }
+    }
+
+    public static <E extends @Nullable Object> HashSet<E> newHashSetWithExpectedSize(
+            int expectedSize) {
+        return new HashSet<>(SetMapLinker.capacity(expectedSize));
+    }
+
     // Extrait de Sets.java (Ligne 405)
     static <E> Set<E> newIdentityHashSet() {
         return Collections.newSetFromMap(new IdentityHashMap<E, Boolean>());
@@ -85,72 +162,6 @@ final class SetMapLinker {
     }
 
     /**
-     * Returns the elements of {@code unfiltered} that satisfy a predicate. The returned set is a live
-     * view of {@code unfiltered}; changes to one affect the other.
-     *
-     * <p>The resulting set's iterator does not support {@code remove()}, but all other set methods
-     * are supported. When given an element that doesn't satisfy the predicate, the set's {@code
-     * add()} and {@code addAll()} methods throw an {@link IllegalArgumentException}. When methods
-     * such as {@code removeAll()} and {@code clear()} are called on the filtered set, only elements
-     * that satisfy the filter will be removed from the underlying set.
-     *
-     * <p>The returned set isn't threadsafe or serializable, even if {@code unfiltered} is.
-     *
-     * <p>Many of the filtered set's methods, such as {@code size()}, iterate across every element in
-     * the underlying set and determine which elements satisfy the filter. When a live view is
-     * <i>not</i> needed, it may be faster to copy {@code Iterables.filter(unfiltered, predicate)} and
-     * use the copy.
-     *
-     * <p><b>Warning:</b> {@code predicate} must be <i>consistent with equals</i>, as documented at
-     * {@link Predicate#apply}. Do not provide a predicate such as {@code
-     * Predicates.instanceOf(ArrayList.class)}, which is inconsistent with equals. (See {@link
-     * Iterables#filter(Iterable, Class)} for related functionality.)
-     *
-     * <p><b>Java 8+ users:</b> many use cases for this method are better addressed by {@link
-     * java.util.stream.Stream#filter}. This method is not being deprecated, but we gently encourage
-     * you to migrate to streams.
-     */
-    // TODO(kevinb): how to omit that last sentence when building GWT javadoc?
-    public static <E extends @Nullable Object> Set<E> filter(
-            Set<E> unfiltered, Predicate<? super E> predicate) {
-        if (unfiltered instanceof SortedSet) {
-            return filter((SortedSet<E>) unfiltered, predicate);
-        }
-        if (unfiltered instanceof Sets.FilteredSet) {
-            // Support clear(), removeAll(), and retainAll() when filtering a filtered
-            // collection.
-            Sets.FilteredSet<E> filtered = (Sets.FilteredSet<E>) unfiltered;
-            Predicate<E> combinedPredicate = com.google.common.base.Predicates.and(filtered.predicate, predicate);
-            return new Sets.FilteredSet<>((Set<E>) filtered.unfiltered, combinedPredicate);
-        }
-
-        return new Sets.FilteredSet<>(checkNotNull(unfiltered), checkNotNull(predicate));
-    }
-
-    /**
-     * Returns an unmodifiable view of the specified navigable set. This method allows modules to
-     * provide users with "read-only" access to internal navigable sets. Query operations on the
-     * returned set "read through" to the specified set, and attempts to modify the returned set,
-     * whether direct or via its collection views, result in an {@code UnsupportedOperationException}.
-     *
-     * <p>The returned navigable set will be serializable if the specified navigable set is
-     * serializable.
-     *
-     * <p><b>Java 8+ users and later:</b> Prefer {@link Collections#unmodifiableNavigableSet}.
-     *
-     * @param set the navigable set for which an unmodifiable view is to be returned
-     * @return an unmodifiable view of the specified navigable set
-     * @since 12.0
-     */
-    public static <E extends @Nullable Object> NavigableSet<E> unmodifiableNavigableSet(
-            NavigableSet<E> set) {
-        if (set instanceof ImmutableCollection || set instanceof Sets.UnmodifiableNavigableSet) {
-            return set;
-        }
-        return new Sets.UnmodifiableNavigableSet<>(set);
-    }
-
-    /**
      * Décomposition d'une méthode hybride (commande + requête).
      * Sépare la recherche des éléments à supprimer de l'action de suppression.
      */
@@ -158,6 +169,16 @@ final class SetMapLinker {
         List<Object> toRemove = findElementsToRemove(set, iterator);
         return performRemoval(set, toRemove);
     }
+
+    /**
+     * {@link AbstractSet} substitute without the potentially-quadratic {@code removeAll}
+     * implementation.
+     */
+    abstract static class ImprovedAbstractSet<E extends @Nullable Object> extends AbstractSet<E> {
+        @Override
+        public boolean removeAll(Collection<?> c) {
+            return removeAllImpl(this, c.iterator());
+        }
 
     /** Identifie les éléments présents dans les deux structures */
     private static List<Object> findElementsToRemove(Set<?> set, Iterator<?> iterator) {
@@ -179,25 +200,6 @@ final class SetMapLinker {
         return set.removeAll(elements);
     }
 
-    /**
-     * {@link AbstractSet} substitute without the potentially-quadratic {@code removeAll}
-     * implementation.
-     */
-    abstract static class ImprovedAbstractSet<E extends @Nullable Object> extends AbstractSet<E> {
-        @Override
-        public boolean removeAll(Collection<?> c) {
-            return removeAllImpl(this, c.iterator());
-        }
 
-        @Override
-        public boolean retainAll(Collection<?> c) {
-            return super.retainAll(checkNotNull(c)); // GWT compatibility
-        }
-    }
-
-    public static <E extends @Nullable Object> HashSet<E> newHashSetWithExpectedSize(
-            int expectedSize) {
-        return new HashSet<>(SetMapLinker.capacity(expectedSize));
-    }
 
 }
