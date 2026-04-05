287c11d38f27b06c6bd3f4e1aa3053a0d9353a1e
Wassim LaMesker <wassim.cherabi.etu@univ-lille.fr>
2026-04-05 19:47:02 +0200

    Revert "Moyenne Modification : Réduction de la compléxité cyclomatique de combinations dans SetsMath"
    This reverts commit 2861dc2d902fdd4c834645d28f21488a1c0bd926.

--- guava/src/com/google/common/collect/SetsMath.java
+++ guava/src/com/google/common/collect/SetsMath.java
@@ -44,17 +44,15 @@ public final class SetsMath {
      * @since 23.0
      */
     public static <E> Set<Set<E>> combinations(Set<E> set, int size) {
-        ImmutableMap<E, Integer> index = SetMapLinker.indexMap(set); // Utilise ton Linker
+        ImmutableMap<E, Integer> index = Maps.indexMap(set);
         checkNonnegative(size, "size");
         checkArgument(size <= index.size(), "size (%s) must be <= set.size() (%s)", size, index.size());
-
         if (size == 0) {
             return ImmutableSet.of(ImmutableSet.of());
         } else if (size == index.size()) {
             return ImmutableSet.of(index.keySet());
         }
-
-        return new SetMapLinker.ImprovedAbstractSet<Set<E>>() { // Utilise ImprovedAbstractSet
+        return new AbstractSet<Set<E>>() {
             @Override
             public boolean contains(@Nullable Object o) {
                 if (o instanceof Set) {
@@ -66,12 +64,74 @@ public final class SetsMath {
 
             @Override
             public Iterator<Set<E>> iterator() {
-                return new CombinationIterator<>(index, size);
+                return new AbstractIterator<Set<E>>() {
+                    final BitSet bits = new BitSet(index.size());
+
+                    @Override
+                    protected @Nullable Set<E> computeNext() {
+                        if (bits.isEmpty()) {
+                            bits.set(0, size);
+                        } else {
+                            int firstSetBit = bits.nextSetBit(0);
+                            int bitToFlip = bits.nextClearBit(firstSetBit);
+
+                            if (bitToFlip == index.size()) {
+                                return endOfData();
+                            }
+
+                            /*
+                             * The current set in sorted order looks like
+                             * {firstSetBit, firstSetBit + 1, ..., bitToFlip - 1, ...}
+                             * where it does *not* contain bitToFlip.
+                             *
+                             * The next combination is
+                             *
+                             * {0, 1, ..., bitToFlip - firstSetBit - 2, bitToFlip, ...}
+                             *
+                             * This is lexicographically next if you look at the combinations in descending order
+                             * e.g. {2, 1, 0}, {3, 1, 0}, {3, 2, 0}, {3, 2, 1}, {4, 1, 0}...
+                             */
+
+                            bits.set(0, bitToFlip - firstSetBit - 1);
+                            bits.clear(bitToFlip - firstSetBit - 1, bitToFlip);
+                            bits.set(bitToFlip);
+                        }
+                        BitSet copy = (BitSet) bits.clone();
+                        return new AbstractSet<E>() {
+                            @Override
+                            public boolean contains(@Nullable Object o) {
+                                Integer i = index.get(o);
+                                return i != null && copy.get(i);
+                            }
+
+                            @Override
+                            public Iterator<E> iterator() {
+                                return new AbstractIterator<E>() {
+                                    int i = -1;
+
+                                    @Override
+                                    protected @Nullable E computeNext() {
+                                        i = copy.nextSetBit(i + 1);
+                                        if (i == -1) {
+                                            return endOfData();
+                                        }
+                                        return index.keySet().asList().get(i);
+                                    }
+                                };
+                            }
+
+                            @Override
+                            public int size() {
+                                return size;
+                            }
+                        };
+                    }
+                };
             }
 
             @Override
             public int size() {
-                return com.google.common.math.IntMath.binomial(index.size(), size);
+                return IntMath.binomial(index.size(), size);
             }
 
             @Override
@@ -107,76 +167,6 @@ public final class SetsMath {
      */
     public static <E> Set<Set<E>> powerSet(Set<E> set) {
         return new PowerSet<E>(set);
-    }
-
-
-    /** Itérateur extrait pour réduire la complexité de SetsMath.combinations */
-    static class CombinationIterator<E> extends AbstractIterator<Set<E>> {
-        private final ImmutableMap<E, Integer> index;
-        private final int size;
-        private final BitSet bits;
-
-        CombinationIterator(ImmutableMap<E, Integer> index, int size) {
-            this.index = index;
-            this.size = size;
-            this.bits = new BitSet(index.size());
-        }
-
-        @Override
-        protected @Nullable Set<E> computeNext() {
-            if (bits.isEmpty()) {
-                bits.set(0, size);
-            } else {
-                int firstSetBit = bits.nextSetBit(0);
-                int bitToFlip = bits.nextClearBit(firstSetBit);
-
-                if (bitToFlip == index.size()) {
-                    return endOfData();
-                }
-
-                bits.set(0, bitToFlip - firstSetBit - 1);
-                bits.clear(bitToFlip - firstSetBit - 1, bitToFlip);
-                bits.set(bitToFlip);
-            }
-            BitSet copy = (BitSet) bits.clone();
-            return new CombinationSet<>(index, copy, size);
-        }
-    }
-
-    /** Classe de support pour représenter une combinaison unique */
-    static class CombinationSet<E> extends SetMapLinker.ImprovedAbstractSet<E> {
-        private final ImmutableMap<E, Integer> index;
-        private final BitSet bits;
-        private final int size;
-
-        CombinationSet(ImmutableMap<E, Integer> index, BitSet bits, int size) {
-            this.index = index;
-            this.bits = bits;
-            this.size = size;
-        }
-
-        @Override
-        public boolean contains(@Nullable Object o) {
-            Integer i = index.get(o);
-            return i != null && bits.get(i);
-        }
-
-        @Override
-        public Iterator<E> iterator() {
-            return new AbstractIterator<E>() {
-                int i = -1;
-                @Override
-                protected @Nullable E computeNext() {
-                    i = bits.nextSetBit(i + 1);
-                    return (i == -1) ? endOfData() : index.keySet().asList().get(i);
-                }
-            };
-        }
-
-        @Override
-        public int size() {
-            return size;
-        }
     }
 
 }
